e5d984612a919725fdc0b97d2227d384e7f6769e
mustofa adny <adniadni04@gmail.com>
2025-06-03 13:28:46 +0700

    math operations

---
+++ math-operations.go
@@ -0,0 +1,17 @@
+package main
+
+func MathOperations() {
+	a := 10
+	b := 9
+	c := a + b
+	d := a - b
+	e := a * b
+	f := a / b
+	g := a % b
+
+	println("Penjumlahan:", c)
+	println("Penjumlahan:", d)
+	println("Penjumlahan:", e)
+	println("Penjumlahan:", f)
+	println("Penjumlahan:", g)
+}
