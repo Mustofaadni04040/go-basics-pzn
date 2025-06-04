8673487c756e54b0449767c6e8507db37e710169
mustofa adny <adniadni04@gmail.com>
2025-06-04 12:26:40 +0700

    function with return value

--- function.go
+++ function.go
@@ -9,3 +9,16 @@ func sayHello() {
 func paramater(name string) {
 	fmt.Println("Hello", name)
 }
+
+// Function with return value
+func getHello(name string) string {
+	return "Hello" + name
+}
+
+func result() {
+	name := "Mustofa"
+	result := getHello(name)
+
+	fmt.Println(result)
+}
+
