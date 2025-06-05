bbff87c81de6533b767e39bc20c23920730c964c
mustofa adny <adniadni04@gmail.com>
2025-06-05 16:06:45 +0700

    package & import

--- helloworld.go
+++ helloworld.go
@@ -1,9 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"belajar-golang-dasar/helper"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello, world!")
 	Slice()
 	SliceFunctions()
+
+	result := helper.SayHello("Mustofa Adny")
+	fmt.Println(result)
 }
