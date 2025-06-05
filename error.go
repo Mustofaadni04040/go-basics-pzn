09a1245edf27b56298c53a7d337f25734655f390
mustofa adny <adniadni04@gmail.com>
2025-06-05 16:31:29 +0700

    error

---
+++ error.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func Pembagian(nilai int, pembagi int) (int, error) {
+	if pembagi == 0 {
+		return 0, errors.New("pembagi tidak boleh nol")
+	} else {
+		return nilai / pembagi, nil
+	}
+}
+
+func exampleMain() {
+	hasil, err := Pembagian(100, 10)
+
+	// error checking
+	if err == nil {
+		fmt.Println(hasil)
+	} else {
+		fmt.Println(err.Error())
+	}
+}
